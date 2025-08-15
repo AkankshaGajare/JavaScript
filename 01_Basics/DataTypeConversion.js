c7afca7e635307c581277bb9649fa9ef2fda7172
AkankshaGajare <akankshagajare74@gmail.com>
2025-08-15 07:47:49 +0000

    DataTypeConversion

---
+++ 01_Basics/DataTypeConversion.js
@@ -0,0 +1,11 @@
+let score = 55
+console.log(score)
+
+let scoreInString = String(score)
+console.log(typeof scoreInString)
+console.log(scoreInString)
+
+let value = 1
+console.log(typeof value)
+
+let char = true
